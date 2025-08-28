d21805c5f893e91eaf8e426d05bafcedd7fe7c0e
Gustavo Castro <45861294+GustavoVCastro@users.noreply.github.com>
2025-08-28 16:13:53 -0300

    New op.: square root

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def home():
 @app.get("/soma/")
 def soma(x: int, y: int):
     return {"soma": x+y}
+
+@app.get("/raiz-quadrada/")
+def raiz_quadrada(radicando: int):
+    return {f"raiz quadrada de {radicando}": radicando ** (1/2)}
